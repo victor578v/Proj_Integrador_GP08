b4bc7e3349a9dd638206a46cf36d768cc1b3bb0b
victor578v <victorandreimaciel67@gmail.com>
2024-07-17 17:09:45 -0300

    Criacao do carousel

--- script/script.js
+++ script/script.js
@@ -1,8 +1,36 @@
-document.getElementById('burger_menu').addEventListener('click', function() {
-    let navbar = document.getElementById('navbar');
-    if (navbar.style.display === 'none') {
-        navbar.style.display = 'block';
-    } else {
-        navbar.style.display = 'none';
-    }
-});
+const burger_menu = document.querySelector("#burger_menu");
+const navbar = document.querySelector("#navbar");
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide, 3000);
+
+burger_menu.addEventListener("click", () =>{
+    navbar.classList.toggle("mostra");
+})
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+
+    const carousel = document.querySelector('.carousel');
+    carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function nextSlide() {
+    currentSlide++;
+    showSlide(currentSlide);
+    resetTimer();
+}
+
+function prevSlide() {
+    currentSlide--;
+    showSlide(currentSlide);
+    resetTimer();
+}
+
+function resetTimer() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 3000);
+}
+
+showSlide(currentSlide);
